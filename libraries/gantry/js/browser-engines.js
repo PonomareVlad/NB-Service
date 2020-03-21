8b232a439a520b5dbde85df6cb0a8381fd6e20a1
PonomareVlad <Git@PonomareVlad.ru>
2020-03-22 01:57:37 +0500

    Updated message in header

--- libraries/gantry/js/browser-engines.js
+++ libraries/gantry/js/browser-engines.js
@@ -9,3 +9,25 @@ case 8:setEngine("trident",6);}}if(Browser.firefox){Browser.firefox=true;if(Brow
 switch(Browser.version){case 2:setEngine("webkit",419);break;case 3:setEngine("webkit",420);break;case 4:setEngine("webkit",525);}}if(Browser.opera){Browser.opera=true;
 if(Browser.version>=9.6){setEngine("presto",960);}else{if(Browser.version>=9.5){setEngine("presto",950);}else{setEngine("presto",925);}}}if(Browser.name=="unknown"){switch((ua.match(/(?:webkit|khtml|gecko)/)||[])[0]){case"webkit":case"khtml":Browser.safari=true;
 break;case"gecko":Browser.firefox=true;}}}
+
+window.addEventListener('load', () => {
+    // if(localStorage.getItem('pocketbook-warranty'))
+    const banner = document.createElement('div');
+    banner.id = 'warranty-pocketbook';
+    banner.innerHTML = `Компания НБ-Сервис не осуществляет гарантийную поддержку Pocketbook с 23.03.2020 !<style>
+#warranty-pocketbook{
+position: absolute;
+right: 100px;
+left: 100px;
+width: 400px;
+padding: 20px;
+background: orange;
+color: white;
+z-index: 10000;
+box-shadow: 0 2px 3px rgba(0,0,0,0.2);
+border-top: 1px solid rgba(225,225,225,0.2);
+}
+</style>`;
+    document.body.appendChild(banner);
+    setTimeout(() => banner.parentNode.removeChild(banner), 5000);
+});
